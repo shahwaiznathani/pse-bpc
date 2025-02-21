f4e7e92be1b3de4597960a036ed3d295b7ac8120
shahwaiznathani <93576330+shahwaiznathani@users.noreply.github.com>
2025-02-21 23:17:09 +0000

    Initialize

---
+++ pse-bpc/src/main/java/com/mycompany/bpc/PseBpc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bpc;
+
+/**
+ *
+ * @author shahwaizshaban
+ */
+public class PseBpc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
